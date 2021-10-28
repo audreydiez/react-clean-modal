a586083ec7801c1d9f862c7616357f6ac67adf0c
Audrey Diez <audrey.diez@gmail.com>
2021-10-28 23:09:07 +0200

    doc: jsdoc add to useModal.jsx

--- src/lib/utils/useModal.jsx
+++ src/lib/utils/useModal.jsx
@@ -1,16 +1,33 @@
 import { useState } from 'react'
 
+/**
+ * A React hook that allows functional components modal
+ * to open and close and optionnal hook for display spinner.
+ *
+ * @returns {boolean} isShowing - Modal showed
+ * @returns {boolean} isShowingSpinner - Spinner showed
+ * @returns {function} toggle - Modal toggle
+ * @returns {function} toggleSpinner - Spinner toggle
+ */
 const useModal = () => {
     const [isShowing, setIsShowing] = useState(false)
     const [isShowingSpinner, setIsShowingSpinner] = useState(false)
 
+    /**
+     * Function for toggle modal
+     */
     function toggle() {
         setIsShowing(!isShowing)
+
+        // Hide spinner when modal isShowing
         if (!isShowing) {
             setIsShowingSpinner(false)
         }
     }
 
+    /**
+     * Function for toggle spinner
+     */
     function toggleSpinner() {
         setIsShowingSpinner(!isShowingSpinner)
     }
